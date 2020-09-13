2536a65ec69d798abf2f23ebb11bb011897989a6
Deivison Cardoso <deivisondc@gmail.com>
2020-09-13 18:59:04 -0300

    add react-router-dom

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+// import { Container } from './styles';
+
+function Home() {
+  return (
+    <motion.div>
+      <header>
+        <h1>Teste</h1>
+      </header>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, voluptatibus! Pariatur, non nostrum aperiam at minima similique, animi sunt deserunt voluptate aliquam, ratione explicabo est omnis laboriosam culpa molestiae nihil?</p>
+    </motion.div>
+  );
+}
+
+export default Home;
